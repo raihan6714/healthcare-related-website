3d1a17cfc7bc856e82df13c15caea05b026602fe
raihan400 <ab40096@gmail.com>
2021-10-17 23:55:08 +0600

    create project and install firebase,react bootstrep,react router and made folders

---
+++ src/components/Shared/FooterLinks/FooterLinks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterLinks = () => {
+    return (
+        <div>
+            <h2>this is footer links</h2>
+        </div>
+    );
+};
+
+export default FooterLinks;
